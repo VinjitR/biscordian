807b376878ef299d9a87cfc4af8b8736c712bbb9
VinjitR <sregulag@usc.edu>
2024-09-06 00:12:18 -0700

    Hiding configs

--- src/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-export default firebaseConfig = {
-    apiKey: "AIzaSyDA3K_pZvZvkgsSp3-cO2mOU_Z8xJm8Rt4",
-    authDomain: "biscordian.firebaseapp.com",
-    projectId: "biscordian",
-    storageBucket: "biscordian.appspot.com",
-    messagingSenderId: "472158568223",
-    appId: "1:472158568223:web:6c49ba84c1d24c2a7c7a2a",
-    measurementId: "G-QLR3M3ME2K"
-  };
