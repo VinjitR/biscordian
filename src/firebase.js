92de481f048119a66bb37ca8081a36e9a5daf40f
VinjitR <sregulag@usc.edu>
2024-09-06 00:01:03 -0700

    Hiding key details

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,6 @@
 import firebase from 'firebase';
 
-
-const firebaseConfig = {
-    apiKey: "AIzaSyDA3K_pZvZvkgsSp3-cO2mOU_Z8xJm8Rt4",
-    authDomain: "biscordian.firebaseapp.com",
-    projectId: "biscordian",
-    storageBucket: "biscordian.appspot.com",
-    messagingSenderId: "472158568223",
-    appId: "1:472158568223:web:6c49ba84c1d24c2a7c7a2a",
-    measurementId: "G-QLR3M3ME2K"
-  };
-
+import firebaseConfig from './firebaseConfig';
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
 
